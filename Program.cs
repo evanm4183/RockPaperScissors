e1b0d9ddf465e1a91a946c313a5820be06a66ff3
Evan Mitchell <evanm4183@gmail.com>
2022-08-18 16:18:26 -0500

    added print and prompt functions

--- Program.cs
+++ Program.cs
@@ -6,7 +6,32 @@ namespace RockPaperScissors
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int playerScore = 0;
+            int computerScore = 0;
+
+            PrintScore(playerScore, computerScore);
+            int userHand = PromptUser();
+
+            Console.WriteLine(userHand);
+        }
+
+        static void PrintScore(int score1, int score2)
+        {
+            Console.WriteLine("------------------------------");
+            Console.WriteLine($"| Player: {score1} | Computer: {score2} |");
+            Console.WriteLine("------------------------------");
+        }
+
+        static int PromptUser()
+        {
+            Console.WriteLine("What would you like to throw?");
+            Console.WriteLine("1) Rock");
+            Console.WriteLine("2) Paper");
+            Console.WriteLine("3) Scissors");
+
+            int playedHand = int.Parse(Console.ReadLine());
+
+            return playedHand;
         }
     }
 }
